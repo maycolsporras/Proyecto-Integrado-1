17fc3b7587368e906b488b83641923ef7539a9c3
Maycol Porras <maycolporras09@icloud.com>
2026-04-02 10:54:55 -0600

    [new]: creando carpetas para inicializar el desarrollo del proyecto

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Importación de rutas
+
+
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=> console.log('MongoDB Atlas conectado'))
+.catch(error => console.log('Ocurrió un error al conectarse con MongoDB: ', error));
+
+// Rutas
+
+
+app.get('/', (req,res)=> {
+    res.send('Servidor en funcionamiento');
+});
+
+app.listen(PORT, ()=>{
+    console.log('Servidor corriendo en http://localhost:' + PORT);
+});
