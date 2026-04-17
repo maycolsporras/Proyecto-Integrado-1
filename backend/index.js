ff99a49a0d399701d4905119b80be9f2b90645ba
Maycol Porras <maycolporras09@icloud.com>
2026-04-17 13:52:59 -0600

    [new]: agregando URI

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cors());
 
+require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
 mongoose.connect(process.env.MONGODB_URI)
 .then(()=> console.log('MongoDB Atlas conectado'))
 .catch(error => console.log('Ocurrió un error al conectarse con MongoDB: ', error));
